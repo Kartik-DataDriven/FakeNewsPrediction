4600d933f0236194344a82685b0414b44a0c6711
Kartik <155235410+Kartik-DataDriven@users.noreply.github.com>
2024-02-13 22:14:13 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,13 +4,13 @@ import re
 import numpy as np
 
 # Load the exported model using pickle
-model_load_path = "../model/logisticreg_model.pkl"
+model_load_path = "./model/logisticreg_model.pkl"
 
 with open(model_load_path, 'rb') as file:
     loaded_model = pickle.load(file)
 
 # Load the TF-IDF vectorizer used during training
-vectorizer_path = "../model/tfidf_vectorizer.pkl"
+vectorizer_path = "./model/tfidf_vectorizer.pkl"
 with open(vectorizer_path, 'rb') as vectorizer_file:
     tfidf_vectorizer = pickle.load(vectorizer_file)
 
@@ -38,9 +38,10 @@ def predict_fake_news(text, vectorizer, model):
 st.title("Fake News Prediction App")
 
 st.markdown("##### Hey i am Kartik (13 years old) and i have built this ML Project which is Fake News Prediction in which i have used 2 ML Models Logistic Regression and Random Forest")
-st.markdown("##### \n-The dataset used in this project is: https://www.kaggle.com/datasets/jainpooja/fake-news-detection")
-st.markdown("##### \n-My Linkedin Profile: https://www.linkedin.com/in/kartik-singh-codolearn/")
-st.markdown("##### \n-My Kaggle Profile: https://www.kaggle.com/kartikexe")
+st.markdown("##### \n-The dataset used in this project is: [Fake News Detection Dataset](https://www.kaggle.com/datasets/jainpooja/fake-news-detection)")
+st.markdown("##### \n- The merged dataset contains a collection of news articles labeled as either 'fake' or 'true,' sourced from two separate files. Each article includes a title, text, subject category, date of publication, and a class label indicating its authenticity. Spanning from August to December 2017, the dataset covers diverse topics including domestic and international politics, with mentions of prominent figures like Donald Trump and Pope Francis. This dataset provides valuable insights into the landscape of news reporting during that time period, offering potential avenues for analyzing the spread of misinformation and the dissemination of factual information.")
+st.markdown("##### \n-My Linkedin Profile: [Kartik Singh - LinkedIn](https://www.linkedin.com/in/kartik-singh-codolearn/)")
+st.markdown("##### \n-My Kaggle Profile: [Kartikexe - Kaggle](https://www.kaggle.com/kartikexe)")
 
 
 # Input text box
